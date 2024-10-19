a3ecfbb0738ff4e3776967c3fd0b8fcf624b38e1
letnhis <arkad.sh@yandex.ru>
2024-10-19 14:57:17 +0700

    added navigation

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.abspath("../src"))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -16,7 +18,6 @@ release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-sys.path.insert(0, os.path.abspath("../src"))
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -35,5 +36,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "classic"
+html_theme = "alabaster"
 html_static_path = ["_static"]
